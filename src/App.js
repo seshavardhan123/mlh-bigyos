a1bed4b56b79cd6bc6a895fccdae9c4248d303fd
seshavardhan123 <58582271+seshavardhan123@users.noreply.github.com>
2019-12-07 01:45:22 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -45,46 +45,59 @@ const config = {
 export default function App() {
   return (
     <SearchProvider config={config}>
-      <WithSearch mapContextToProps={({ wasSearched }) => ({ wasSearched })}>
-        {({ wasSearched }) => {
-          return (
-            <div className="App">
-              <ErrorBoundary>
-                <Layout
-                  header={<SearchBox autocompleteSuggestions={true} />}
-                  sideContent={
-                    <div>
-                      {wasSearched && (
-                        <Sorting
-                          label={"Sort by"}
-                          sortOptions={buildSortOptionsFromConfig()}
-                        />
-                      )}
-                      {getFacetFields().map(field => (
-                        <Facet key={field} field={field} label={field} />
-                      ))}
-                    </div>
-                  }
-                  bodyContent={
-                    <Results
-                      titleField={getConfig().titleField}
-                      urlField={getConfig().urlField}
-                      shouldTrackClickThrough={true}
-                    />
-                  }
-                  bodyHeader={
-                    <React.Fragment>
-                      {wasSearched && <PagingInfo />}
-                      {wasSearched && <ResultsPerPage />}
-                    </React.Fragment>
-                  }
-                  bodyFooter={<Paging />}
-                />
-              </ErrorBoundary>
-            </div>
-          );
-        }}
-      </WithSearch>
+//       <WithSearch mapContextToProps={({ wasSearched }) => ({ wasSearched })}>
+//         {({ wasSearched }) => {
+//           return (
+//             <div className="App">
+//               <ErrorBoundary>
+//                 <Layout
+//                   header={<SearchBox autocompleteSuggestions={true} />}
+//                   sideContent={
+//                     <div>
+//                       {wasSearched && (
+//                         <Sorting
+//                           label={"Sort by"}
+//                           sortOptions={buildSortOptionsFromConfig()}
+//                         />
+//                       )}
+//                       {getFacetFields().map(field => (
+//                         <Facet key={field} field={field} label={field} />
+//                       ))}
+//                     </div>
+//                   }
+//                   bodyContent={
+//                     <Results
+//                       titleField={getConfig().titleField}
+//                       urlField={getConfig().urlField}
+//                       shouldTrackClickThrough={true}
+//                     />
+//                   }
+//                   bodyHeader={
+//                     <React.Fragment>
+//                       {wasSearched && <PagingInfo />}
+//                       {wasSearched && <ResultsPerPage />}
+//                     </React.Fragment>
+//                   }
+//                   bodyFooter={<Paging />}
+//                 />
+//               </ErrorBoundary>
+//             </div>
+//           );
+//         }}
+//       </WithSearch>
+    {({ results }) => {
+    return (
+      <div>
+        {results.map(result => (
+          <Result key={result.id.raw}
+            result={result}
+            titleField="title"
+            urlField="nps_link"
+          />
+        ))}
+      </div>
+    );
+  }}
     </SearchProvider>
   );
 }
